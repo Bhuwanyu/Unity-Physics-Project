0863e02c85283ae2fe840a8b5c7515f57b529ab2
Bhuwanyu Agrawal <agrabhuwanyu@gmail.com>
2025-12-26 16:55:19 +0530

    commit

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SceneManager.LoadScene(0);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SceneManager.LoadScene(1);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            SceneManager.LoadScene(2);
+    }
+}
